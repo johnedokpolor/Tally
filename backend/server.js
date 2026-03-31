aec92a8c08c7a98a44bcd55aafac96a1aa3d9b98
John Edokpolor <johnedokpolor@gmail.com>
2026-03-31 16:17:05 +0100

    fixed post routes

--- backend/server.js
+++
@@ -1,7 +0,0 @@
-const content = "Hello to @itachi @sasuke and my friend @garra";
-const mentions =
-  content
-    .match(/@\w+/g)
-    ?.map((mention) => mention.toLowerCase().replace("@", "")) || [];
-
-console.log(mentions);
