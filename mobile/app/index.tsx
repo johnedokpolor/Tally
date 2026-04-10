0445c1f7539680323b95507f32728a1d67296e87
John Edokpolor <johnedokpolor@gmail.com>
2026-04-10 19:01:52 +0100

    native wind added

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,29 +1,33 @@
+import "./global.css";
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "pink",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-red-500">
+        Welcome to Nativewind! things will change from now on and forever
+      </Text>
+      <View className="w-52 h-52 bg-pink-500"></View>
+      <View className="w-52 h-52 bg-violet-500"></View>
+      <View className="w-52 h-52 bg-red-500"></View>
+      <View className="w-52 h-52 bg-blue-500"></View>
+      <View className="w-52 h-52 bg-red-500"></View>
+      <View className="w-52 h-52 bg-blue-500"></View>
+      <View className="w-52 h-52 bg-red-500"></View>
+      <View className="w-52 h-52 bg-blue-500"></View>
+      <View className="w-52 h-52 bg-red-500"></View>
+      <View className="w-52 h-52 bg-blue-500"></View>
+      <View className="w-52 h-52 bg-red-500"></View>
+      <View className="w-52 h-52 bg-blue-500"></View>
+      <View className="w-52 h-52 bg-red-500"></View>
+      <View className="w-52 h-52 bg-blue-500"></View>
+      <View className="w-52 h-52 bg-red-500"></View>
+      <View className="w-52 h-52 bg-blue-500"></View>
+      <View className="w-52 h-52 bg-red-500"></View>
+      <View className="w-52 h-52 bg-blue-500"></View>
+      <View className="w-52 h-52 bg-red-500"></View>
+      <View className="w-52 h-52 bg-blue-500"></View>
+      <View className="w-52 h-52 bg-red-500"></View>
     </View>
   );
 }
