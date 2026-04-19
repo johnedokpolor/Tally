9fb0374225658ea5e05e4424ffb8d119e6dc6849
John Edokpolor <johnedokpolor@gmail.com>
2026-04-19 20:13:29 +0100

    clerk auth added

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,6 +1,20 @@
-import { Stack } from "expo-router";
+import { ClerkProvider } from "@clerk/expo";
+import { tokenCache } from "@clerk/expo/token-cache";
+import { Slot, Stack } from "expo-router";
 import "./global.css";
 
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error("Add your Clerk Publishable Key to the .env file");
+}
+
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <Stack>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      </Stack>
+    </ClerkProvider>
+  );
 }
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,33 +1,15 @@
-import "./global.css";
-import { Text, View } from "react-native";
+import { useClerk } from "@clerk/expo";
+import React from "react";
+import { View, Text, Button } from "react-native";
 
-export default function App() {
+const HomeScreen = () => {
+  const { signOut } = useClerk();
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-red-500">
-        Welcome to Nativewind! things will change from now on and forever
-      </Text>
-      <View className="w-52 h-52 bg-pink-500"></View>
-      <View className="w-52 h-52 bg-violet-500"></View>
-      <View className="w-52 h-52 bg-red-500"></View>
-      <View className="w-52 h-52 bg-blue-500"></View>
-      <View className="w-52 h-52 bg-red-500"></View>
-      <View className="w-52 h-52 bg-blue-500"></View>
-      <View className="w-52 h-52 bg-red-500"></View>
-      <View className="w-52 h-52 bg-blue-500"></View>
-      <View className="w-52 h-52 bg-red-500"></View>
-      <View className="w-52 h-52 bg-blue-500"></View>
-      <View className="w-52 h-52 bg-red-500"></View>
-      <View className="w-52 h-52 bg-blue-500"></View>
-      <View className="w-52 h-52 bg-red-500"></View>
-      <View className="w-52 h-52 bg-blue-500"></View>
-      <View className="w-52 h-52 bg-red-500"></View>
-      <View className="w-52 h-52 bg-blue-500"></View>
-      <View className="w-52 h-52 bg-red-500"></View>
-      <View className="w-52 h-52 bg-blue-500"></View>
-      <View className="w-52 h-52 bg-red-500"></View>
-      <View className="w-52 h-52 bg-blue-500"></View>
-      <View className="w-52 h-52 bg-red-500"></View>
+    <View>
+      <Text>HomeScreen</Text>
+      <Button onPress={() => signOut()} title="Logout"></Button>
     </View>
   );
-}
+};
+
+export default HomeScreen;
